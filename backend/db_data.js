479abd7f8da9dba4f0e08e24d13eaa690555e3ae
Iustin <iustin.esanu7@gmail.com>
2024-02-27 22:39:41 +0200

    feat: database generic data to file for script

---
+++ backend/db_data.js
@@ -0,0 +1,104 @@
+export const genreData = [
+  {
+    genreName: "Action",
+    description:
+      "Exciting and fast-paced movies with intense physical activities and battles.",
+  },
+  {
+    genreName: "Drama",
+    description:
+      "Character-driven stories that explore emotional and interpersonal conflicts.",
+  },
+  {
+    genreName: "Comedy",
+    description: "Entertaining films designed to amuse and provoke laughter.",
+  },
+  {
+    genreName: "Sci-Fi",
+    description:
+      "Speculative fiction that explores imaginative and futuristic concepts.",
+  },
+  {
+    genreName: "Thriller",
+    description:
+      "Suspenseful and gripping movies designed to keep the audience on the edge of their seats.",
+  },
+  {
+    genreName: "Horror",
+    description:
+      "Movies that evoke fear and suspense, often involving supernatural elements.",
+  },
+  {
+    genreName: "Romance",
+    description:
+      "Love stories that focus on romantic relationships and emotions.",
+  },
+  {
+    genreName: "Mystery",
+    description:
+      "Movies that involve solving a puzzle or uncovering hidden secrets.",
+  },
+  {
+    genreName: "Fantasy",
+    description:
+      "Films set in magical or fantastical worlds with elements of magic and mythical creatures.",
+  },
+  {
+    genreName: "Documentary",
+    description:
+      "Non-fiction films that present real-life events, people, and topics.",
+  },
+];
+
+export const peopleData = [
+  {
+    personName: "Quentin Tarantino",
+    birthDate: "1963-03-27",
+    description: "American filmmaker, screenwriter, producer, and actor.",
+  },
+  {
+    personName: "John Travolta",
+    birthDate: "1954-02-18",
+    description:
+      "American actor, producer, dancer, and singer. Known for Pulp Fiction.",
+  },
+  {
+    personName: "Samuel L. Jackson",
+    birthDate: "1948-12-21",
+    description: "American actor and producer. Known for Pulp Fiction.",
+  },
+  {
+    personName: "Christopher Nolan",
+    birthDate: "1970-07-30",
+    description: "English film director, producer, and screenwriter.",
+  },
+  {
+    personName: "Greta Gerwig",
+    birthDate: "1983-08-04",
+    description: "American actress, playwright, screenwriter, and director.",
+  },
+  {
+    personName: "Margot Robbie",
+    birthDate: "1990-07-02",
+    description:
+      "Australian actress and film producer. Known for her roles in movies like Suicide Squad.",
+  },
+  {
+    personName: "Ryan Gosling",
+    birthDate: "1980-11-12",
+    description:
+      "Canadian actor and musician. Known for his roles in movies like La La Land.",
+  },
+];
+
+export const contentTypeData = [
+  { content_type_name: "movie" },
+  { content_type_name: "tv-series" },
+  { content_type_name: "documentary" },
+  { content_type_name: "reality-show" },
+];
+
+export const usersData = [
+  { username: "Ben Dover", bio: "This is James Ben Dover" },
+  { username: "Walter White", bio: "Jesse let's cook" },
+];
