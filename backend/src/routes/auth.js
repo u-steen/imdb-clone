441355de04b72497e1fba59945574d34ee2c0c9d
Iustin <iustin.esanu7@gmail.com>
2024-03-08 11:37:50 +0200

    feat: login checking

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -33,6 +33,30 @@ router.post("/auth/register", async (req, res) => {
   registerUser({ username, email, birth_date, password_hash, salt, bio });
   res.sendStatus(200);
 });
+
+router.post("/auth/login", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.sendStatus(404);
+  }
+  const foundUser = await getUser(username);
+  if (!foundUser) {
+    res.send(401);
+    return;
+  }
+  const hashAlg = createHash("sha256");
+  const saltedPass = password + foundUser[0].salt;
+  hashAlg.update(saltedPass);
+  const providedHashedPass = hashAlg.digest("hex");
+  if (providedHashedPass === foundUser[0].password_hash) {
+    res.sendStatus(200);
+    // Aici suntem logati
+    return;
+  }
+  res.sendStatus(401);
+  // console.log("Provided pass afteer hash\t", providedHashedPass);
+  // console.log("Passowrd from db\t\t", foundUser[0].password_hash);
+});
 // TODO: Delete user
 // TODO: Modify user data
 
